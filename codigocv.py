323417bfe280619517cc8488d397a9fdd865d0bc
Marcio Leite <marciosantanaleite@gmail.com>
2026-02-20 04:00:07 -0300

    nw para codigos do cv

---
+++ codigocv.py
@@ -0,0 +1,12 @@
+
+
+sexo = input ('Qual é o seu sexo?')
+idade = input ('Qual é a sua idade?')
+
+print ('Olá cabra,' ,sexo, 'tu tem,' ,idade,'isso aí memu é?')
+
+# creditos copilot que me ajudou n virgula antes das variáveis
+
+
+
+
